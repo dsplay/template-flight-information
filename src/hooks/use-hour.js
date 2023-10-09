0bda653de8bdad32cff9819d8b553a43a34b0cf5
Pedro R <rochapedro436@gmail.com>
2023-10-09 13:17:53 -0300

    Hooks updating

--- src/hooks/use-hour.js
+++ src/hooks/use-hour.js
@@ -2,7 +2,7 @@ import { useConfig } from '@dsplay/react-template-utils';
 
 export default function hourFormat() {
   const { locale } = useConfig();
-  if (locale === 'en_US') {
+  if (locale === 'en') {
     return true;
   }
   return false;
