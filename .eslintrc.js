68d6854ee02b784c8fc8596cea0bd2f5b438e988
Pedro R <rochapedro436@gmail.com>
2023-10-09 11:30:59 -0300

    linebreadk ctrlf

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
     'no-unused-vars': ['error', { 'args': 'none' }],
     'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'off',
+    'linebreak-style': ['error', 'windows'],
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
   },
