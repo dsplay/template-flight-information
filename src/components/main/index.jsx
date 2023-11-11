351446e747cafadb61e3e0ed68c49b5b827ad640
Paulo Coutinho <pcmnac@gmail.com>
2023-11-11 10:26:26 +0000

    ♻️ hide time of last update

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -16,9 +16,9 @@ const formattedUpdateTime = {
   year: 'numeric',
   month: 'long',
   day: '2-digit',
-  hour: '2-digit',
-  minute: '2-digit',
-  second: '2-digit',
+  // hour: '2-digit',
+  // minute: '2-digit',
+  // second: '2-digit',
 };
 const dateOptions = {
   hour: '2-digit',
